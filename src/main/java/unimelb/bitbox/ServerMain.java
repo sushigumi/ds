9187e4fc5b11d2baff9319146d4f06459a360409
wz <jljc0323@gmail.com>
2019-04-19 01:50:06 +1000

    Added an array list of connections for reference in ConnectionManager

--- src/main/java/unimelb/bitbox/ServerMain.java
+++ src/main/java/unimelb/bitbox/ServerMain.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
 
 import unimelb.bitbox.connection.ConnectionManager;
