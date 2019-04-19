3c603fa3fd950fa6b9588c467a478713f8d5e98d
wz <jljc0323@gmail.com>
2019-04-19 18:43:52 +1000

    Added a new runnable for FileBytesRequest

--- src/main/java/unimelb/bitbox/runnables/FileBytesRequest.java
+++ src/main/java/unimelb/bitbox/runnables/FileBytesRequest.java
@@ -1,19 +1,34 @@
 package unimelb.bitbox.runnables;
 
+import unimelb.bitbox.messages.MessageGenerator;
+import unimelb.bitbox.util.Document;
 import unimelb.bitbox.util.FileSystemManager;
 
 import java.io.DataOutputStream;
+import java.util.ArrayList;
 
+/**
+ * This Runnable is queued on the sender thread after a File Create Request has been sent and the file
+ * needs to be copied from the peer.
+ * Sends a series of messages depending on how big the file that needs to requested is and also the blockSize
+ * to receive the content of the file.
+ */
 public class FileBytesRequest extends BaseRunnable {
-    private FileSystemManager manager;
+    private Document fileDescriptor;
+    private String pathName;
 
-    public FileBytesRequest(DataOutputStream output, FileSystemManager manager) {
+    public FileBytesRequest(DataOutputStream output, Document fileDescriptor, String pathName) {
         super(output);
-        this.manager = manager;
+        this.fileDescriptor = fileDescriptor;
+        this.pathName = pathName;
     }
 
     @Override
     public void run() {
+        ArrayList<String> messages = MessageGenerator.genFileBytesRequests(fileDescriptor, pathName);
 
+        for (String message : messages) {
+            sendMessage(message);
+        }
     }
 }
