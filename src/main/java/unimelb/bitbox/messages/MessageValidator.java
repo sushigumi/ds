89920b4cbb7362d9b3440618b1fc937105d74b70
Yan Li <muzifeng1021@yahoo.com>
2019-04-25 23:21:58 +1000

    update invalid protocol

--- src/main/java/unimelb/bitbox/messages/MessageValidator.java
+++ src/main/java/unimelb/bitbox/messages/MessageValidator.java
@@ -12,17 +12,174 @@ public class MessageValidator {
     private MessageValidator() {
     }
 
-    public String validateFileCreateRequest(Document doc) {
-        if (doc.getString("command") == null) {
-            return "message must contain a command field as string";
+    public String validateFileChangeRequest(Document doc) {
+    	
+    	Document fileDescriptor = (Document)doc.get("fileDescriptor");
+    	
+        if (doc.getString("fileDescriptor") == null) {
+        	return "fileDescriptor";       	
         }
-        if (doc.getString())
+        else if (fileDescriptor.getString("md5") == null) {
+        	return "md5";
+        }
+        //not so sure
+        else if (Long.valueOf(fileDescriptor.getLong("fileSize") )== null) {
+        	return "fileSize";	
+        }
+        
+        else if (Long.valueOf(fileDescriptor.getLong("lastModified")) == null) {
+        	return "lastModified";	
+        }
+        
+        if (doc.getString("pathName") == null) {
+        	return "pathName";       	
+        }
 
         //return error message
         return null;
     }
 
-    public String validateFileCreateResponse(Document doc) {
-
+    public String validateFileChangeResponse(Document doc) {
+    	
+    	Document fileDescriptor = (Document)doc.get("fileDescriptor");
+    	
+        if (doc.getString("fileDescriptor") == null) {
+        	return "fileDescriptor";       	
+        }
+        else if (fileDescriptor.getString("md5") == null) {
+        	return "md5";
+        }
+        //not so sure
+        else if (Long.valueOf(fileDescriptor.getLong("fileSize") )== null) {
+        	return "fileSize";	
+        }
+        
+        else if (Long.valueOf(fileDescriptor.getLong("lastModified")) == null) {
+        	return "lastModified";	
+        }
+        
+        if (doc.getString("pathName") == null) {
+        	return "pathName";       	
+        }
+        
+        if (doc.getString("message") == null) {
+        	return "message";       	
+        }
+        
+        if (Boolean.toString(doc.getBoolean("status")) == null) {
+        	return "status";     	
+        }
+   	
+		return null;
+		
     }
+    
+    public String validateFileBytesRequest(Document doc) {
+    	
+        Document fileDescriptor = (Document)doc.get("fileDescriptor");
+    	
+        if (doc.getString("fileDescriptor") == null) {
+        	return "fileDescriptor";       	
+        }
+        else if (fileDescriptor.getString("md5") == null) {
+        	return "md5";
+        }
+        //not so sure
+        else if (Long.valueOf(fileDescriptor.getLong("fileSize") )== null) {
+        	return "fileSize";	
+        }
+        
+        else if (Long.valueOf(fileDescriptor.getLong("lastModified")) == null) {
+        	return "lastModified";	
+        }
+        
+        if (doc.getString("pathName") == null) {
+        	return "pathName";       	
+        }
+    	
+        if (Long.valueOf(doc.getLong("position") )== null) {
+        	return "position";	
+        }
+        
+        if (Long.valueOf(doc.getLong("length")) == null) {
+        	return "length";	
+        }
+        
+        return null;	
+    	
+    }
+    
+    public String validateFileBytesResponse(Document doc) {
+    	
+        Document fileDescriptor = (Document)doc.get("fileDescriptor");
+    	
+        if (doc.getString("fileDescriptor") == null) {
+        	return "fileDescriptor";       	
+        }
+        else if (fileDescriptor.getString("md5") == null) {
+        	return "md5";
+        }
+        //not so sure
+        else if (Long.valueOf(fileDescriptor.getLong("fileSize") )== null) {
+        	return "fileSize";	
+        }
+        
+        else if (Long.valueOf(fileDescriptor.getLong("lastModified")) == null) {
+        	return "lastModified";	
+        }
+        
+        if (doc.getString("pathName") == null) {
+        	return "pathName";       	
+        }
+    	
+        if (Long.valueOf(doc.getLong("position") )== null) {
+        	return "position";	
+        }
+        
+        if (Long.valueOf(doc.getLong("length")) == null) {
+        	return "length";	
+        }
+        
+        if (doc.getString("content") == null) {
+        	return "content";       	
+        }        
+        
+        if (doc.getString("message") == null) {
+        	return "message";       	
+        }
+        
+        if (Boolean.toString(doc.getBoolean("status")) == null) {
+        	return "status";     	
+        }
+        return null;	
+	 	
+    }
+    
+    
+    public String validateDirectoryChangeRequest(Document doc) {
+    	
+    	 if (doc.getString("pathName") == null) {
+         	return "pathName";       	
+         }
+    	 else {
+    		 return null;
+    	 }
+    	
+    }
+    public String validateDirectoryChangeResponse(Document doc) {
+    	
+    	 if (doc.getString("pathName") == null) {
+         	return "pathName";       	
+         }
+    	
+    	 if (doc.getString("message") == null) {
+         	return "message";       	
+         }
+         
+         if (Boolean.toString(doc.getBoolean("status")) == null) {
+         	return "status";     	
+         }
+         return null;	
+    }
+    
 }
